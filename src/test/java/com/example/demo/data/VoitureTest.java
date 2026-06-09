c0932eddb71017372eeb62eb1edf34cb6c556f63
Hachimohammed <mohahac112@gmail.com>
2026-06-09 15:22:02 +0200

    TEST2

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -10,15 +10,13 @@ public class VoitureTest {
 
     @Test
     void testCreerVoitureConstructeurVide() {
-        // Initialisation
+       
         Voiture voiture = new Voiture();
-        
-        // Action (utilisation des setters)
+       
         voiture.setMarque("Renault");
         voiture.setPrix(15000);
         voiture.setId(1);
 
-        // Vérification
         assertEquals("Renault", voiture.getMarque(), "La marque doit être Renault");
         assertEquals(15000, voiture.getPrix(), "Le prix doit être 15000");
         assertEquals(1, voiture.getId(), "L'id doit être 1");
@@ -28,8 +26,6 @@ public class VoitureTest {
     void testCreerVoitureAvecParametres() {
         
         Voiture voiture = new Voiture("Peugeot", 20000);
-
-       
         assertEquals("Peugeot", voiture.getMarque(), "La marque doit être Peugeot");
         assertEquals(20000, voiture.getPrix(), "Le prix doit être 20000");
     }
